9f10d8c24a1d327c9b847b46d18c1911fc233e91
begonia-jan <begonia-jan@looptelecom.com>
2025-07-14 22:53:38 +0800

    run 2010/01/01~2019/12/31 data

---
+++ code/mergeTcsv.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from pathlib import Path
+
+# 資料夾路徑
+root_folder = Path(r"C:\Users\USER\Desktop\Han\學習\金融策略\分析程式\data\analysis\momentumNew")
+
+# 找到所有 07-t_test.csv
+all_files = list(root_folder.rglob("07-t_test.csv"))
+
+# 儲存最終整合結果
+final_df = pd.DataFrame()
+
+for file in all_files:
+    # 讀取檔案
+    df = pd.read_csv(file)
+    
+    # 根據路徑取得 oPeriod 與 hPeriod
+    parts = file.parts
+    folder_name = [p for p in parts if "oPeriod" in p and "_hPeriod" in p]
+    if folder_name:
+        name_part = folder_name[0]
+        o_period = name_part.split("_")[0].replace("oPeriod", "")
+        h_period = name_part.split("_")[1].replace("hPeriod", "")
+    else:
+        o_period = ""
+        h_period = ""
+    
+    # 新增欄位
+    df["oPeriod"] = o_period
+    df["hPeriod"] = h_period
+    
+    # 合併進總表
+    final_df = pd.concat([final_df, df], ignore_index=True)
+
+savePath = r"C:\Users\USER\Desktop\Han\學習\金融策略\分析程式\data\analysis\momentumNew\mergeTtestResult\merged_t_test.csv"
+# 儲存
+final_df.to_csv(savePath, index=False)
