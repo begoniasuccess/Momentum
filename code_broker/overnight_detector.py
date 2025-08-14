19ef45e09cf5afeb13d414f33065b849259e2065
begonia-jan <begonia-jan@looptelecom.com>
2025-08-14 17:36:26 +0800

    delete no use code

---
+++ code_broker/overnight_detector.py
@@ -0,0 +1,29 @@
+import psycopg2
+import pandas as pd
+
+# 連線資料庫
+def connect_db() -> psycopg2.extensions.connection:
+    conn = psycopg2.connect(
+        host='140.113.87.91',
+        database='finDB',
+        user='nycu_findb_user',
+        password='NYCUd@t@b@se8791'
+    )
+    conn.autocommit = True
+    return conn
+
+
+# 讀取資料表：tw_broker_daily_bs_stock_b
+if __name__ == '__main__':
+    conn = connect_db()
+    sql =   f"""
+            SELECT *
+                        FROM public.tw_broker_daily_bs_stock_b
+                        WHERE securities_trader_id = '9268' AND 
+                                    stock_id = '2330'
+                        ORDER BY DATE DESC
+                        LIMIT 100
+            """
+    df = pd.read_sql_query(sql, conn)
+    
+    
