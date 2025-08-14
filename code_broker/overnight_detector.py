7b80d0e8ed904387712d126dcdd4f492535ce6a9
begonia-jan <begonia-jan@looptelecom.com>
2025-08-14 18:19:17 +0800

    test data

--- code_broker/overnight_detector.py
+++ code_broker/overnight_detector.py
@@ -17,13 +17,15 @@ def connect_db() -> psycopg2.extensions.connection:
 if __name__ == '__main__':
     conn = connect_db()
     sql =   f"""
-            SELECT *
-                        FROM public.tw_broker_daily_bs_stock_b
-                        WHERE securities_trader_id = '9268' AND 
-                                    stock_id = '2330'
-                        ORDER BY DATE DESC
-                        LIMIT 100
+            SELECT * FROM public.tw_broker_daily_bs_stock_b
+                WHERE securities_trader_id = '9268' 
+                    AND stock_id = '2330'
+                ORDER BY DATE DESC
+                LIMIT 100
             """
+            
     df = pd.read_sql_query(sql, conn)
+    df.to_csv("test.csv")
     
+    print(df.head())
     
